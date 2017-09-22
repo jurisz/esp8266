58c31f4fd7cc0f93c4561478e60868a5bd96a4f0
juris.zubkans <juris.zubkans>
2017-09-22 20:16:58 +0300

    working sensor sample on gpo-2

--- experiments/sensorDataSender/sensorDataSender.ino
+++ experiments/sensorDataSender/sensorDataSender.ino
@@ -8,7 +8,7 @@
 #include <MyWifiSecurity.h>
 
 #define USE_SERIAL Serial
-#define DHTPIN 1     // what pin we're connected to
+#define DHTPIN 2     // what pin we're connected to
 #define DHTTYPE DHT22   // DHT 22  (AM2302)
 
 DHT dht(DHTPIN, DHTTYPE);
@@ -53,23 +53,26 @@ void connectAndPostData(SensorData data) {
     if((WiFiMulti.run() == WL_CONNECTED)) {
         HTTPClient http;
         USE_SERIAL.println("http begin connect");
-        http.begin("http://192.168.20.1:3000/sensor/save/c1");
+        char url[128];
+        sprintf(url, "http://192.168.20.1:3000/sensor/save/%s", GREEN_HOUSE_CODE); 
+        USE_SERIAL.print("url: ");
+        USE_SERIAL.println(url);
+        http.begin(url);
 
         USE_SERIAL.println("Http posting");
         // start connection and send HTTP header
         http.addHeader("Accept", "*/*");
         http.addHeader("Content-Type", "application/json");
         
-        float temp = (float) random(500)/10.0;
         char tempStr[7];
         char humidStr[7];
         char jsonBuffer[255];
         sprintf(jsonBuffer, "[{\"name\": \"temp\", \"value\": %s}, {\"name\": \"humidity\", \"value\": %s}]",
          formatFloat(data.temp, tempStr), formatFloat(data.humidity, humidStr));
 
-//        USE_SERIAL.print("json payload: ");
-//        USE_SERIAL.print(jsonBuffer);
-//        USE_SERIAL.print("\n");
+        //USE_SERIAL.print("json payload: ");
+        //USE_SERIAL.print(jsonBuffer);
+        //USE_SERIAL.print("\n");
         
         int httpCode = http.POST(jsonBuffer);
 
@@ -100,20 +103,23 @@ void setup() {
     USE_SERIAL.println();
     USE_SERIAL.println();
 
-   for(uint8_t t = 4; t > 0; t--) {
+   for(uint8_t t = 5; t > 0; t--) {
         USE_SERIAL.printf("[SETUP] WAIT %d...\n", t);
         USE_SERIAL.flush();
         delay(1000);
    }
 
    //TODO switch on sensor power GPIO-2	
-   
-   delay(SENSOR_WARM_UP_SECONDS * 1000);
+   //delay(SENSOR_WARM_UP_SECONDS * 1000);
    
    SensorData data = readSensor();
-   connectAndPostData(data);
+   if (isnan(data.humidity) || isnan(data.temp)) {
+      USE_SERIAL.println("No sensor data");
+   } else {
+      connectAndPostData(data); 
+   }
 
-   USE_SERIAL.println("Measurements done and sent. Going for deep sleep...");
+   USE_SERIAL.println("Measurements completed and sent. Going for deep sleep...");
    ESP.deepSleep(DEEP_SLEEP_SECONDS * 1000000);
 }
 
